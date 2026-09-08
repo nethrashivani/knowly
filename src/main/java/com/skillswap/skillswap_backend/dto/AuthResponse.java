5f6ee754ccb5ecbe45120d36c507e9c793e87032
nethrashivani <110420538+nethrashivani@users.noreply.github.com>
2026-09-08 20:49:08 +0530

    feat(auth): add authentication response DTO

---
+++ src/main/java/com/skillswap/skillswap_backend/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.skillswap.skillswap_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String name;
+    private String email;
+}
