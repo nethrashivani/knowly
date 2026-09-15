60ca89d3f139d79a04d309c958487fb28f88e006
nethrashivani <110420538+nethrashivani@users.noreply.github.com>
2026-09-15 20:02:02 +0530

    Add workshop review API

--- knowly-frontend/src/services/ratingService.js
+++ knowly-frontend/src/services/ratingService.js
@@ -14,6 +14,16 @@ export const getRatingsForUser = async (userId) => {
   return response.data;
 };
 
+export const getRatingsForWorkshop = async (workshopId) => {
+  const response = await axios.get(`${BASE_URL}/workshop/${workshopId}`);
+  return response.data;
+};
+
+export const getMyRatingForWorkshop = async (workshopId) => {
+  const response = await axios.get(`${BASE_URL}/workshop/${workshopId}/mine`, authHeaders());
+  return response.data;
+};
+
 export const createRating = async (ratingData) => {
   const response = await axios.post(BASE_URL, ratingData, authHeaders());
   return response.data;
