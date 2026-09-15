c51ea844f6433f0b74442928870d8f539e4105a8
nethrashivani <110420538+nethrashivani@users.noreply.github.com>
2026-09-15 20:58:02 +0530

    Use profile-first skills page

--- knowly-frontend/src/App.jsx
+++ knowly-frontend/src/App.jsx
@@ -10,7 +10,7 @@ import ProfilePage from './pages/ProfilePage';
 import PublicProfilePage from './pages/PublicProfilePage';
 import MySkillsPage from './pages/MySkillsPage';
 import MyInterestsPage from './pages/MyInterestsPage';
-import SkillsPage from './pages/SkillsPage';
+import SkillsExplorePage from './pages/SkillsExplorePage';
 import WorkshopsPage from './pages/WorkshopsPage';
 import WorkshopDetailsPage from './pages/WorkshopDetailsPage';
 import CreateWorkshopPage from './pages/CreateWorkshopPage';
@@ -24,41 +24,35 @@ import RateWorkshopPage from './pages/RateWorkshopPage';
 import RoomPage from './pages/RoomPage';
 import MyRoomsPage from './pages/MyRoomsPage';
 
-function ProtectedRoute({ children }) {
-  return isLoggedIn() ? children : <Navigate to="/login" />;
-}
+function ProtectedRoute({ children }) { return isLoggedIn() ? children : <Navigate to="/login" />; }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-        <Route path="/skills" element={<ProtectedRoute><SkillsPage /></ProtectedRoute>} />
-        <Route path="/skills/:id" element={<ProtectedRoute><SkillDetailsPage /></ProtectedRoute>} />
-        <Route path="/skills/:id/demand" element={<ProtectedRoute><SkillDemandPage /></ProtectedRoute>} />
-        <Route path="/add" element={<ProtectedRoute><AddSkillPage /></ProtectedRoute>} />
-        <Route path="/edit/:id" element={<ProtectedRoute><EditSkillPage /></ProtectedRoute>} />
-        <Route path="/my-skills" element={<ProtectedRoute><MySkillsPage /></ProtectedRoute>} />
-        <Route path="/my-interests" element={<ProtectedRoute><MyInterestsPage /></ProtectedRoute>} />
-        <Route path="/users/:userId" element={<ProtectedRoute><PublicProfilePage /></ProtectedRoute>} />
-        <Route path="/workshops" element={<ProtectedRoute><WorkshopsPage /></ProtectedRoute>} />
-        <Route path="/workshops/:workshopId" element={<ProtectedRoute><WorkshopDetailsPage /></ProtectedRoute>} />
-        <Route path="/workshops/create" element={<ProtectedRoute><CreateWorkshopPage /></ProtectedRoute>} />
-        <Route path="/my-workshops" element={<ProtectedRoute><MyWorkshopsPage /></ProtectedRoute>} />
-        <Route path="/my-applications" element={<ProtectedRoute><MyApplicationsPage /></ProtectedRoute>} />
-        <Route path="/workshops/:workshopId/applications" element={<ProtectedRoute><ManageApplicationsPage /></ProtectedRoute>} />
-        <Route path="/workshops/:workshopId/resources" element={<WorkshopResourcesPage />} />
-        <Route path="/my-applications/:applicationId" element={<MyApplicationsPage />} />
-        <Route path="/workshop-history" element={<WorkshopHistoryPage />} />
-        <Route path="/workshops/:workshopId/rate" element={<RateWorkshopPage />} />
-        <Route path="/my-rooms" element={<ProtectedRoute><MyRoomsPage /></ProtectedRoute>} />
-        <Route path="/room" element={<ProtectedRoute><RoomPage /></ProtectedRoute>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+    <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+    <Route path="/skills" element={<ProtectedRoute><SkillsExplorePage /></ProtectedRoute>} />
+    <Route path="/skills/:id" element={<ProtectedRoute><SkillDetailsPage /></ProtectedRoute>} />
+    <Route path="/skills/:id/demand" element={<ProtectedRoute><SkillDemandPage /></ProtectedRoute>} />
+    <Route path="/add" element={<ProtectedRoute><AddSkillPage /></ProtectedRoute>} />
+    <Route path="/edit/:id" element={<ProtectedRoute><EditSkillPage /></ProtectedRoute>} />
+    <Route path="/my-skills" element={<ProtectedRoute><MySkillsPage /></ProtectedRoute>} />
+    <Route path="/my-interests" element={<ProtectedRoute><MyInterestsPage /></ProtectedRoute>} />
+    <Route path="/users/:userId" element={<ProtectedRoute><PublicProfilePage /></ProtectedRoute>} />
+    <Route path="/workshops" element={<ProtectedRoute><WorkshopsPage /></ProtectedRoute>} />
+    <Route path="/workshops/:workshopId" element={<ProtectedRoute><WorkshopDetailsPage /></ProtectedRoute>} />
+    <Route path="/workshops/create" element={<ProtectedRoute><CreateWorkshopPage /></ProtectedRoute>} />
+    <Route path="/my-workshops" element={<ProtectedRoute><MyWorkshopsPage /></ProtectedRoute>} />
+    <Route path="/my-applications" element={<ProtectedRoute><MyApplicationsPage /></ProtectedRoute>} />
+    <Route path="/workshops/:workshopId/applications" element={<ProtectedRoute><ManageApplicationsPage /></ProtectedRoute>} />
+    <Route path="/workshops/:workshopId/resources" element={<WorkshopResourcesPage />} />
+    <Route path="/my-applications/:applicationId" element={<MyApplicationsPage />} />
+    <Route path="/workshop-history" element={<WorkshopHistoryPage />} />
+    <Route path="/workshops/:workshopId/rate" element={<RateWorkshopPage />} />
+    <Route path="/my-rooms" element={<ProtectedRoute><MyRoomsPage /></ProtectedRoute>} />
+    <Route path="/room" element={<ProtectedRoute><RoomPage /></ProtectedRoute>} />
+  </Routes></BrowserRouter>;
 }
 
 export default App;
