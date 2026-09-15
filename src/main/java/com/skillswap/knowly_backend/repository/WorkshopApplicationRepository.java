b71799fe27353661f745fff37a12a062b44740df
nethrashivani <110420538+nethrashivani@users.noreply.github.com>
2026-09-15 21:32:25 +0530

    Prepare workshop deletion cleanup

--- src/main/java/com/skillswap/knowly_backend/repository/WorkshopApplicationRepository.java
+++ src/main/java/com/skillswap/knowly_backend/repository/WorkshopApplicationRepository.java
@@ -6,20 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface WorkshopApplicationRepository
-        extends JpaRepository<WorkshopApplication, Long> {
-
+public interface WorkshopApplicationRepository extends JpaRepository<WorkshopApplication, Long> {
     List<WorkshopApplication> findByLearner_Email(String email);
-
     List<WorkshopApplication> findByWorkshop_Id(Long workshopId);
-
-    Optional<WorkshopApplication> findByWorkshop_IdAndLearner_Email(
-            Long workshopId,
-            String email
-    );
-
-    Optional<WorkshopApplication> findByWorkshop_IdAndLearner_Id(
-            Long workshopId,
-            Long learnerId
-    );
+    Optional<WorkshopApplication> findByWorkshop_IdAndLearner_Email(Long workshopId, String email);
+    Optional<WorkshopApplication> findByWorkshop_IdAndLearner_Id(Long workshopId, Long learnerId);
 }
