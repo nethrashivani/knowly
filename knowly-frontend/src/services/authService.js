1274b40debf98b12f5482e82a4dc29891b9bf8bb
nethrashivani <nethrashivanims@gmail.com>
2026-09-15 14:27:56 +0530

    Fix production auth API URL

--- knowly-frontend/src/services/authService.js
+++ knowly-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://https://knowly-lphd.onrender.com/api/auth';
+const BASE_URL = 'https://knowly-lphd.onrender.com/api/auth';
 
 export const register = async (data) => {
     const response = await axios.post(
