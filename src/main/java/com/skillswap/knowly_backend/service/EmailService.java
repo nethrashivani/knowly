918ffc6e48239992756f0ede706363ea204aed69
nethrashivani <110420538+nethrashivani@users.noreply.github.com>
2026-09-16 00:58:19 +0530

    Fix EmailService for Spring Boot 4 Jackson 3

--- src/main/java/com/skillswap/knowly_backend/service/EmailService.java
+++ src/main/java/com/skillswap/knowly_backend/service/EmailService.java
@@ -1,6 +1,6 @@
 package com.skillswap.knowly_backend.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
